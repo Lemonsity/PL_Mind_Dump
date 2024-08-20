b21afe21e233d8cdc986b9a04c1b35aad8b088c9
Lemon <38927629+Lemonsity@users.noreply.github.com>
2024-08-20 19:58:29 -0400

    Add direct translation of canonical list version of my-pmembero

--- The-Little-Series/The-Reasoned-Schemer/Prolog-Version/myPmemberoProlog.pl
+++ The-Little-Series/The-Reasoned-Schemer/Prolog-Version/myPmemberoProlog.pl
@@ -1,5 +1,5 @@
 % -*- Mode: Prolog -*-
-:- module(myPmemberoProlog, [myPmembero/2]).
+:- module(myPmemberoProlog, [myPmembero/2, myPmemberoCan/2]).
 
 % Implementation using build in [length]
 myPmemberoHelper(X, [X | T], TailLength) :- length(T, TailLength).
@@ -9,3 +9,15 @@ myPmemberoStartWith(X, L, Len) :- myPmemberoHelper(X, L, Len).
 myPmemberoStartWith(X, L, Len) :- LenAdd1 is Len + 1, myPmemberoStartWith(X, L, LenAdd1).
 
 myPmembero(X, L) :- myPmemberoStartWith(X, L, 0).
+
+% Implementation using canonical list
+myLengtho([], []).
+myLengtho([_ | T1], [unit | T2]) :- myLengtho(T1, T2).
+
+myPmemberoHelperCan(X, [X | T], Len) :- myLengtho(T, Len).
+myPmemberoHelperCan(X, [_ | T], [unit | NewLen]) :- myPmemberoHelperCan(X, T, NewLen).
+
+myPmemberoStartWithCan(X, L, Len) :- myPmemberoHelperCan(X, L, Len).
+myPmemberoStartWithCan(X, L, Len) :- myPmemberoStartWithCan(X, L, [unit | Len]).
+
+myPmemberoCan(X, L) :- myPmemberoStartWithCan(X, L, []).
