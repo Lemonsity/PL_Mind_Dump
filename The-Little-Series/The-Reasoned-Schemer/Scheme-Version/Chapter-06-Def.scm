92d474b041baaf17c22cfceb763285f0c1a9a8e8
Lemon <38927629+Lemonsity@users.noreply.github.com>
2024-08-22 00:32:28 -0400

    Finish The Reasoned Schemer Chapter 06

--- The-Little-Series/The-Reasoned-Schemer/Scheme-Version/Chapter-06-Def.scm
+++ The-Little-Series/The-Reasoned-Schemer/Scheme-Version/Chapter-06-Def.scm
@@ -1,1 +1,20 @@
 (load "Chapter-05-Def.scm")
+
+(define anyo
+  (lambda (g)
+    (conde
+     [g succeed]
+     [(anyo g)])))
+
+(define nevero
+  (anyo fail))
+
+(define alwayso
+  (anyo succeed))
+
+(define salo
+  (lambda (g)
+    (conde
+     [succeed succeed]
+     [g])))
+
