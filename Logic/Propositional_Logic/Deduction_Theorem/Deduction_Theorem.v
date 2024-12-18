e4ab00df6f3c90c8faad1dc2dc4f3323390bb6ff
Lemon <38927629+Lemonsity@users.noreply.github.com>
2024-12-18 14:21:59 -0500

    Finish Deduction Theorem
    Moved file into dedicated folder, easier to organize Coq compilation files

--- Logic/Propositional_Logic/Deduction_Theorem/Deduction_Theorem.v
+++ Logic/Propositional_Logic/Deduction_Theorem/Deduction_Theorem.v
@@ -1,6 +1,137 @@
 From Coq Require Import Lists.List. Import ListNotations.
 From Coq Require Import Init.Nat.
 
+Inductive wff : Type :=
+| var (n : nat)
+| negate (a : wff)
+| implies (a : wff) (b : wff).
+
+Definition is_implies_bool (a : wff) : bool :=
+  match a with
+  | implies _ _ => true
+  | _ => false
+  end.
+
+
+
+Fixpoint lift (assignment : nat -> bool) (a : wff) : bool :=
+  match a with
+  | var n => assignment n
+  | negate a' => negb (lift assignment a')
+  | implies a' b' => (negb (lift assignment a')) || (lift assignment b')
+  end.
+
+Definition tautology (a : wff) : Prop :=
+  forall assignment : nat -> bool , lift assignment a = true.
+
+Theorem tautology_1 : forall (a : wff) , tautology (implies a a).
+Proof.
+  intros a assignment.
+  simpl.
+  destruct (lift assignment a) as [ | ].
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Definition wff_2 (a b : wff) : wff := implies b (implies a b).
+
+Theorem tautology_2 : forall (a b : wff) , tautology (wff_2 a b).
+Proof.
+  intros a b assignment.
+  simpl.
+  destruct (lift assignment b) , (lift assignment a).
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Definition wff_3 (a b c : wff) : wff :=
+  implies
+    (implies a c)
+    (implies
+       (implies a (implies c b))
+       (implies a b)).
+
+Theorem tautology_3 : forall (a b c : wff) ,
+      tautology (wff_3 a b c).
+          
+Proof.
+  intros a b c assignment.
+  simpl.
+  destruct (lift assignment a) , (lift assignment b) , (lift assignment c).
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+
+Inductive deduce (gamma : list wff) : wff -> Prop :=
+| is_tauto : forall (a : wff) , (tautology a) -> (deduce gamma a)
+| in_context : forall (a : wff) , (In a gamma) -> (deduce gamma a)
+| mp : forall (a b : wff) , (deduce gamma a) -> (deduce gamma (implies a b)) -> (deduce gamma b).
+
+Theorem deduction_theorem : forall (gamma : list wff) (a b : wff) (tree : deduce (a :: gamma) b) ,
+    deduce gamma (implies a b).
+Proof.
+  intros gamma a b tree.
+  induction tree as
+    [ b' b_is_tauto
+    | b' b_in_context
+    | c b' gamma_deduce_a IH1 gamma_deduce_implies IH2 ].
+  {
+    apply is_tauto.
+    {
+      intros assignment.
+      simpl.
+      rewrite -> b_is_tauto.
+      rewrite -> Bool.orb_true_r.
+      reflexivity.
+    }
+  }
+  {
+    destruct b_in_context as [ a_eq_b | b_in_gamma ].
+    {
+      apply is_tauto.
+      rewrite -> a_eq_b.
+      apply tautology_1.
+    }
+    {
+      apply (mp gamma b'). (* gamma is somehow implicit here? *)
+      {
+        apply (in_context _ _ b_in_gamma).
+      }
+      {
+        apply is_tauto.
+        apply tautology_2.
+      }
+    }
+  }            
+  { 
+    apply (mp _ (implies a (implies c b'))).
+    - apply IH2.
+    - apply (mp _ (implies a c)).
+      + apply IH1.
+      + apply is_tauto.
+        apply (tautology_3 a b' c).
+  }
+Qed.
+
+(** =======================================================================================
+    Below are previous attempt
+
+    I made things much more complicated than necessary
+    But I did got practice with proving decidability
+    =======================================================================================
+ *)
+
+(**
+
 Inductive reflect (P : Prop) : bool -> Prop :=
 | ReflectT (H : P) : reflect P true
 | ReflectF (H : ~ P) : reflect P false.
@@ -14,18 +145,6 @@ Proof.
   - apply ReflectT. apply H. reflexivity.
   - apply ReflectF. unfold not. rewrite <- H. intros contra. discriminate contra.
 Qed.
-
-  
-Inductive wff : Type :=
-| var (n : nat)
-| negate (a : wff)
-| implies (a : wff) (b : wff).
-
-Definition is_implies_bool (a : wff) : bool :=
-  match a with
-  | implies _ _ => true
-  | _ => false
-  end.
 
 Definition is_implies_prop (a : wff) : Prop :=
   exists (a' b' : wff) , a = implies a' b'.
@@ -137,82 +256,24 @@ Proof.
   intros a b.
   apply (iff_reflect _ _ (same_wff_bool_iff_prop a b)).
 Qed.
-
-Fixpoint lift (assignment : nat -> bool) (a : wff) : bool :=
-  match a with
-  | var n => assignment n
-  | negate a' => negb (lift assignment a')
-  | implies a' b' => (negb (lift assignment a')) || (lift assignment b')
-  end.
-
-Definition tautology (a : wff) : Prop :=
-  forall assignment : nat -> bool , lift assignment a = true.
-
-Theorem tautology_1 : forall (a : wff) , tautology (implies a a).
-Proof.
-  intros a assignment.
-  simpl.
-  destruct (lift assignment a) as [ | ].
-  - reflexivity.
-  - reflexivity.
-Qed.
-
-Definition wff_2 (a b : wff) : wff := implies b (implies a b).
-
-Theorem tautology_2 : forall (a b : wff) , tautology (wff_2 a b).
-Proof.
-  intros a b assignment.
-  simpl.
-  destruct (lift assignment b) , (lift assignment a).
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-Qed.
-
-Definition wff_3 (a b c : wff) : wff :=
-  implies
-    (implies a c)
-    (implies
-       (implies a (implies c b))
-       (implies a b)).
-
-Theorem tautology_3 : forall (a b c : wff) ,
-      tautology (wff_3 a b c).
-          
-Proof.
-  intros a b c assignment.
-  simpl.
-  destruct (lift assignment a) , (lift assignment b) , (lift assignment c).
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-  - reflexivity.
-Qed.              
-
-(** The d   
- *)
-Inductive deduce (gamma : list wff) : list wff -> Prop :=
-| start : deduce gamma []
-| is_tauto : forall (history : list wff) (a : wff) ,
-    (deduce gamma history) -> (tautology a) -> deduce gamma (a :: history)
-| in_context : forall (history : list wff) (a : wff) ,
-    (deduce gamma history) -> (In a gamma) -> deduce gamma (a :: history)
-| mp : forall (history history' : list wff) (a b : wff) ,
-    (deduce gamma (a :: history)) -> (deduce gamma ((implies a b) :: history'))
-    -> deduce gamma (b :: ((implies a b) :: history') ++ (a :: history)).
+  
+Inductive deduce' (gamma : list wff) : list wff -> Prop :=
+| start' : deduce' gamma []
+| is_tauto' : forall (history : list wff) (a : wff) ,
+    (deduce' gamma history) -> (tautology a) -> deduce' gamma (a :: history)
+| in_context' : forall (history : list wff) (a : wff) ,
+    (deduce' gamma history) -> (In a gamma) -> deduce' gamma (a :: history)
+| mp' : forall (history history' : list wff) (a b : wff) ,
+    (deduce' gamma (a :: history)) -> (deduce' gamma ((implies a b) :: history'))
+    -> deduce' gamma (b :: ((implies a b) :: history') ++ (a :: history)).
 
 Lemma deduction_theorem_is_tautology : forall (b : wff) ,
-  (tautology b) -> (forall (gamma : list wff) (a : wff) , exists (history : list wff) , deduce gamma (implies a b :: history)).
+  (tautology b) -> (forall (gamma : list wff) (a : wff) , exists (history : list wff) , deduce' gamma (implies a b :: history)).
 Proof.
   intros b b_is_tautology.
   intros gamma a.
   exists [].
-  apply (is_tauto _ _ _ (start _)).
+  apply (is_tauto' _ _ _ (start' _)).
   intros assignment.
   simpl.
   rewrite -> b_is_tautology.
@@ -221,7 +282,7 @@ Proof.
 Qed.
 
 Lemma deduction_theorem_in_context : forall (b : wff) , forall (gamma : list wff) (a : wff) ,
-    (In b (a :: gamma)) -> (exists (history : list wff) , deduce gamma (implies a b :: history)).
+    (In b (a :: gamma)) -> (exists (history : list wff) , deduce' gamma (implies a b :: history)).
 Proof.
   intros b gamma a.
   intros b_in_a_gamma.
@@ -229,27 +290,33 @@ Proof.
   {
     rewrite -> eq.
     exists [].
-    apply is_tauto.
-    apply start.
+    apply is_tauto'.
+    apply start'.
     apply tautology_1.
   }
   {
     simpl in b_in_a_gamma.
     destruct b_in_a_gamma as [ a_eq_b | b_in_gamma ].
-    { destruct (neq a_eq_b). }
-    { 
+    { destruct (neq a_eq_b).    (* contradiction *) }
+    {
       exists [(wff_2 a b) ; b].
-      apply (mp gamma [] []).
+      apply (mp' gamma [] []).
       (* [b] is in [gamma] *)
-      apply (in_context _ _ _ (start _) b_in_gamma).
+      apply (in_context' _ _ _ (start' _) b_in_gamma).
       (* [b -> (a -> b)] is a tautology *)
-      apply (is_tauto _ _ _ (start _) (tautology_2 a b)).
+      apply (is_tauto' _ _ _ (start' _) (tautology_2 a b)).
     }
   }
 Qed.
 
-Theorem deduction_theorem : forall (gamma history : list wff) (a b : wff) ,
-    deduce (a :: gamma) (b :: history) -> (exists (history' : list wff), deduce gamma ((implies a b) :: history')).
+(** This prove ends up being much too complicated *)
+
+(*     The addition of history requires strong induction on list *)
+(*     Much too painful *)
+(*  *)
+
+Theorem deduction_theorem' : forall (gamma history : list wff) (a b : wff) ,
+    deduce' (a :: gamma) (b :: history) -> (exists (history' : list wff), deduce' gamma ((implies a b) :: history')).
 Proof.
   intros gamma history.
   generalize dependent gamma.
@@ -282,15 +349,8 @@ Proof.
       apply (deduction_theorem_in_context b gamma a b_in_a_gamma).
     }
     {
-      assert (deduction_a_c : (exists (history_a_c : list wff) , deduce gamma ((implies a c) :: history_a_c))).
+      assert (deduction_a_c : (exists (history_a_c : list wff) , deduce' gamma ((implies a c) :: history_a_c))).
       {
-        exists 
-        apply IH.
-      }
-    }
-    
-    
-    admit. }
 Admitted.
-  
-  
+
+ *)
