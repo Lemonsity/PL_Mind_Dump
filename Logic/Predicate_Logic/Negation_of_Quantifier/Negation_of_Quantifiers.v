248d87504bf5db6ca2cb3a943dce171bb6fe28f1
Lemon <38927629+Lemonsity@users.noreply.github.com>
2025-04-06 01:04:01 -0400

    Add Note to Negation of Quantifiers

--- Logic/Predicate_Logic/Negation_of_Quantifier/Negation_of_Quantifiers.v
+++ Logic/Predicate_Logic/Negation_of_Quantifier/Negation_of_Quantifiers.v
@@ -1,8 +1,10 @@
-Axiom LEM : forall (P : Prop) , P \/ (~ P).
+Definition LEM := forall (P : Prop) , P \/ (~ P).
 
-Theorem by_contra :
+Theorem LEM__by_contra :
+  LEM ->
   forall (P : Prop) , (~ ~ P) -> P.
 Proof.
+  intros LEM.
   unfold not.
   intros P not_not_P.
   destruct (LEM P) as [evi_P | evi_not_P] .
@@ -11,26 +13,41 @@ Proof.
 Qed.
   
 Theorem neg_forall :
+  LEM ->
   forall (A : Type) (P : A -> Prop) ,
     (~ forall (x : A) , P x) <-> (exists (x : A) , ~ P x).
 Proof.
+  intros LEM.
+  assert (by_contra : forall (P : Prop) , (~ ~ P) -> P).
+  { apply (LEM__by_contra LEM). }
+  
   intros A P.
   split.
   {
-    unfold not.
-    intros forall_Px_lead_contra.
-    apply by_contra.
-    unfold not.
-    intros counter_ex_lead_contra.
+    intros not_all_is_true.
 
-    apply forall_Px_lead_contra.
-    intros x.
+    (** The Challenge
+
+        How would someone choose the witness? *)
+    
     apply by_contra.
-    unfold not.
-    intros counter_ex.
-    assert (exist_counter_ex : exists x : A, P x -> False).
-    { exists x . assumption. }
-    apply (counter_ex_lead_contra exist_counter_ex).
+    intros no_counter_example.
+
+    (** Intuition
+
+        After application (I don't know why...),
+        we need to show [forall x . P x]
+
+        Because we assumed [no_counter_example],
+        for each [x], we can eliminate [~ P x] *)
+
+    apply not_all_is_true.
+    intros x.
+    destruct (LEM (P x)) as [Px | not_Px].
+    - assumption.
+    - exfalso.
+      apply no_counter_example.
+      exists x. apply not_Px.
   }
   {
     intros [x Px_lead_contra] forall_Px.
@@ -39,6 +56,23 @@ Proof.
   }
 Qed.
 
+(** Negating an [exists] Formula
+
+    It turns out we don't need LEM for this proof.
+    In constructive mathematic, negation is represented
+    as the ability to produce [False].
+
+    [->]:
+        We want to show any [x] satisfying [P x]
+        leads to [False].
+        This is immediate because such [P x] immediates
+        witness [exists x . P x]
+    [<-]:
+        Want to show [exists x . P x] leads to contradiction.
+        Well yes, because such witness can be applied to
+        [forall x . P x -> False]
+
+    The process only requires logical implication *)
 Theorem neg_exists :
   forall (A : Type) (P : A -> Prop) ,
     (~ exists (x : A) , P x) <-> (forall (x : A) , ~ P x).
